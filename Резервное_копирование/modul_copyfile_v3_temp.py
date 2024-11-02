32c1c0f59a65cf1353f37f5f1235155e4e98e2be
Igor Vimbor <vimbor73@gmail.com>
2024-11-02 12:28:26 +0300

    Актуальный релиз проекта

--- Резервное_копирование/modul_copyfile_v3_temp.py
+++ Резервное_копирование/modul_copyfile_v3_temp.py
@@ -1,19 +1,13 @@
-# ВЕРСИЯ для автоматического резервного копирования файлов по расписанию,
-# которое настраивается в Планировщике задач Windows
-
-# ------------------- Вспомогательный класс, осуществляющий непосредственное копирование файлов ---------------
-# класс Copy_file взят из модуля modul_copyfile_v3.py
-from tkinter import Tk, Label
-import json
 import os
 import shutil
 from datetime import date, datetime
+
 
 year_now = str(date.today().year)  # текущий год
 data = date.today()  # сегодняшняя дата
 
 # файл для логирования (располагается в каталоге проекта или приложения)
-file_logs = "D:/My Programs/Резервное копирование/Резервное копирование_logs.txt"
+file_logs = "Резервное копирование_logs.txt"
 
 
 class Copy_file:
@@ -58,14 +52,25 @@ class Copy_file:
             temp_dir = f"{self.path_full_name}/temp_archive_dir/"
             os.makedirs(temp_dir, exist_ok=True)
 
-            # циклом по списку файлов для копирования
-            for file in self.files:
-                # отсекаем название файла с расширением
-                file_name = os.path.basename(file)  # file.split("/")[-1]
-                # копируем файл во временный каталог копий файлов
-                shutil.copy(file, os.path.join(temp_dir, file_name))
-                # копируем файл в каталог резевных копий (если требуется, то РАСКОМЕНТИРОВАТЬ)
-                # shutil.copy(file, os.path.join(self.path_full_name, file_name))
+            # # циклом по списку файлов для копирования
+            # for file in self.files:
+            #     # отсекаем название файла с расширением
+            #     file_name = os.path.basename(file)  # file.split("/")[-1]
+            #     # копируем файл во временный каталог копий файлов
+            #     shutil.copy(file, os.path.join(temp_dir, file_name))
+            #     # копируем файл в каталог резевных копий (если требуется, то РАСКОМЕНТИРОВАТЬ)
+            #     # shutil.copy(file, os.path.join(self.path_full_name, file_name))
+
+            # циклом по списку копируем файлы и каталоги во временный каталог
+            for item in self.files:
+                if os.path.isfile(item):  # если объект - это файл
+                    # отсекаем название файла с расширением
+                    file_name = os.path.basename(item)  # file.split("/")[-1]
+                    # копируем файл во временный каталог копий файлов
+                    shutil.copy(item, os.path.join(temp_dir, file_name))
+                elif os.path.isdir(item):  # если объект - это каталог
+                    base_name = os.path.basename(item)
+                    shutil.copytree(item, os.path.join(temp_dir, base_name))
 
             # Создаем архив
             archive_name = f"{self.path_full_name}Архив файлов_{data}"
@@ -91,61 +96,17 @@ class Copy_file:
             return False
 
 
-# --------------------- Основной модуль - считывание из базы данных и старт резервного копирования ---------------
-# Создана общая база данных - файл "Резервное копирование_база данных.txt", в который записывается словарь .json
-# В базе данных по ключу "files" хранится перечень файлов для копирования, по ключу "dirs" - перечень каталогов.
-
-
-# база данных - перечень резервных копий файлов и каталогов
-database = "D:/My Programs/Резервное копирование/Резервное копирование_база данных.txt"
-
-# открываем базу данных, считываем файл json и сохраняем словарь в переменную
-with open(database, encoding="utf-8-sig") as file:
-    dct_files: dict = json.load(file)
-
-# формируем окно Tkinter
-window = Tk()
-# заголовок окна Tkinter
-window.title("Автоматическое Резервное Копирование")
-width = 450  # ширина окна
-heigh = 220  # высота окна
-# определяем координаты центра экрана и размещаем окно по центру
-screenwidth = window.winfo_screenwidth()
-screenheight = window.winfo_screenheight()
-window.geometry(
-    "%dx%d+%d+%d"
-    % (width, heigh, (screenwidth - width) / 2, (screenheight - heigh) / 2)
-)
-
-lst_flag = []  # список флагов при копировании в каждый каталог
-# по каждому каталогу из списка выбранных каталогов
-for dr in dct_files["dirs"]:
-    # создаем экземпляр класса Copy_file
-    obj = Copy_file(dct_files["files"], dr)
-    flag = obj.copy_file()  # копируем файлы в указанный каталог
-    lst_flag.append(flag)  # добавляем флаг в список флагов
-# print(lst_flag)
-
-# вывод информационного окна о сохранении файлов или ошибке
-if all(lst_flag):  # если все флаги в списке True
-    # создаем виджет Label с текстом
-    lbl = Label(
-        window,
-        text="Резервные копии\nфайлов записаны\nпо расписанию.",
-        font=("Arial Bold", 40),
-        fg="green",
-    )
-else:  # иначе
-    lbl = Label(
-        window,
-        text="При резервном копировании\nфайлов по расписанию\nпроизошла ошибка!\nПроверьте списки файлов!",
-        font=("Arial Bold", 35),
-        fg="red",
-    )
-
-# размещаем виджет в окне
-lbl.grid(column=0, row=0)
-
-
 if __name__ == "__main__":
-    window.mainloop()
+
+    files_to_copy = [
+        "E:/EDUCATION/ПЕРЕЧЕНЬ по Python.docx",
+        "E:/YMZ_month_json.txt",
+        "E:/YMZ_year_json.txt",
+    ]
+
+    # корневой каталог для копирования резервных копий файлов
+    path_2 = "E:/"
+
+    obj_2 = Copy_file(files_to_copy, path_2)
+    result = obj_2.copy_file()
+    print(result)
