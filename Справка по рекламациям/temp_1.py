d14e2298bea970f87e49d24b5e035abdc016ba0c
Igor Vimbor <vimbor73@gmail.com>
2025-01-26 21:39:26 +0300

    Изменения в GUI для подготовки справки

--- Справка по рекламациям/temp_1.py
+++ Справка по рекламациям/temp_1.py
@@ -5,8 +5,6 @@ from tkinter import messagebox
 import tkinter.ttk as ttk
 import time
 
-import data_files_modul as dfm
-
 
 def on_button_click():
     """функция запуска создания отчета и записи его в файл с изменением поведения кнопки в процессе"""
