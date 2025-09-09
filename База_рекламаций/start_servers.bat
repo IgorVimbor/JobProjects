f6dc335b1191fa392fd7b6b6f434d6b60e421d25
Igor Vimbor <vimbor73@gmail.com>
2025-09-09 17:26:09 +0300

    Созданы bat файлы для запуска проекта

--- База_рекламаций/start_servers.bat
+++ База_рекламаций/start_servers.bat
@@ -1,20 +1,32 @@
 @echo off
-title Django + Nginx Servers
-echo ========================================
-echo       Запуск серверов
-echo ========================================
+:: Устанавливаем кодировку UTF-8 для корректного отображения русских символов
+chcp 65001 > nul
+title Сервер Django
+echo        ========================================
+echo             Запуск серверов Django + Nginx
+echo        ========================================
 
-echo Запуск Django сервера...
+echo.
+echo        Запуск Django сервера...
+:: Переходим в папку проекта и активируем виртуальное окружение
 cd /d "E:\MyRepositories\JobProjects\База_рекламаций"
 call r-hub_venv\Scripts\activate
 cd reclamationhub
+:: Запускаем сервер Django в режиме продакшен в отдельном минимизированном окне
 start /min "Django" cmd /k "python manage.py runserver 127.0.0.1:8000 --settings=reclamationhub.settings.production"
 
 timeout /t 3 /nobreak > nul
 
-echo Запуск Nginx...
+echo.
+:: Запускаем прокси-сервер Nginx
+echo        Запуск Nginx...
 cd /d "C:\nginx-1.28.0"
-.\nginx.exe
+start "" ".\nginx.exe"
 
-echo Серверы запущены!
+echo.
+:: Выводим информационное сообщение о запуске серверов
 timeout /t 2 /nobreak > nul
+echo        Серверы запущены!
+timeout /t 3 /nobreak > nul
+:: Закрываем окно
+exit
